d4243a3b3519e45bd0f745603716118a8a20486c
ilkash <126604951+ilkash@users.noreply.github.com>
2025-10-29 23:42:11 +0200

    створення проекту та компонентів

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import Footer from "@/copmonents/Footer/Footer";
+import Header from "@/copmonents/Header/Header";
+import React from "react";
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Hero from "@/copmonents/Hero/Hero";
+import HowItWorks from "@/copmonents/HowItWorks/HowItWorks";
+
+export default function HomePage() {
+  return (
+    <div>
+      HomePage
+      <Hero />
+      <HowItWorks />
+    </div>
+  );
+}
