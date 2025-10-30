7d25bbefe05b923ee3f49c1ebba2c4e0930081e7
nasti <anastasiasaienko634@gmail.com>
2025-10-30 22:43:38 +0100

    updt HomePage

--- copmonents/Hero/Hero.tsx
+++ copmonents/Hero/Hero.tsx
@@ -1,3 +1,15 @@
 export default function Hero() {
-  return <div>Hero</div>;
+  return (
+    <section title="Головна частина">
+      <h1>FairHire — чесний найм без понтів</h1>
+      <p>
+        Платформа, яка показує не резюме, а людину в дії. Отримайте справедливу
+        оцінку ваших навичок через реальні завдання.
+      </p>
+      <div>
+        <button>Я кандидат</button>
+        <button>Я компанія</button>
+      </div>
+    </section>
+  );
 }
