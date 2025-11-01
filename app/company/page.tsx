9f63faa9cccbda48fc45d8126ec5c4c7ebb749d9
Alexey <makovii88@gmail.com>
2025-11-01 23:50:21 +0100

    update base style & company page

--- app/company/page.tsx
+++ app/company/page.tsx
@@ -1,11 +1,25 @@
-import CandidateList from "@/copmonents/CandidateList/CandidateList";
-import CompanyPanel from "@/copmonents/CompanyPanel/CompanyPanel";
-import Link from "next/link";
+import CandidateList from '@/copmonents/CandidateList/CandidateList';
+import CompanyPanel from '@/copmonents/CompanyPanel/CompanyPanel';
+import Link from 'next/link';
+import { FiPlus } from 'react-icons/fi';
 
 export default function Company() {
   return (
     <div>
-      <h1>Company Page</h1>
+      <div className="flex justify-between pb-6 pt-12 items-center">
+        <div className="flex flex-col gap-2 flex-dire">
+          <h1>Панель компанії</h1>
+          <p className="text-[#99a1af] font-normal">
+            Знаходьте таланти через їхні дії, не резюме
+          </p>
+        </div>
+        <button
+          type="button"
+          className="flex gap-4 justify-center items-center w-[181px] h-9 rounded-[7px] bg-[#5dd62c] font-medium hover:bg-[#4cc026] transition text-[14px] leading-[1.43] text-[#0f0f0f] cursor-pointer"
+        >
+          <FiPlus size={16} /> Створити вакансію
+        </button>
+      </div>
       <Link href="/"> link home</Link>
       <CompanyPanel />
       <CandidateList />
