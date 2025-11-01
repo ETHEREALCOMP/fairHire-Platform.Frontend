c8d67f88c9a7c51da0f23a22b9015b10a8bd4a46
ilkash <126604951+ilkash@users.noreply.github.com>
2025-11-01 12:27:10 +0200

    настройка стилів + html сторінки реєстрації

--- copmonents/Header/Header.tsx
+++ copmonents/Header/Header.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      Header
+      <Link href="/sing-up">sing up</Link>
+    </div>
+  );
 }
