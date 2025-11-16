cec8519beab46ca632fb0df4b451bfc74500d2d1
nasti <anastasiasaienko634@gmail.com>
2025-11-16 19:57:23 +0100

    update HomePage

--- copmonents/Footer/Footer.tsx
+++ copmonents/Footer/Footer.tsx
@@ -1,3 +1,15 @@
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <>
+      <div className="w-full border-t border-gray-600"></div>
+      <footer
+        aria-label="footer"
+        className="flex justify-center items-center py-7"
+      >
+        <h2 className="text-gray-400">
+          © 2025 FairHire. Чесний найм для всіх.
+        </h2>
+      </footer>
+    </>
+  );
 }
